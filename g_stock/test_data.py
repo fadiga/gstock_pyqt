1deb5259b173804ccf66d525645e50328d6da0b0
Fadiga <ibfadiga@gmail.com>
2011-11-17 16:55:13 +0000

    Amélioration

--- g_stock/test_data.py
+++ g_stock/test_data.py
@@ -12,13 +12,20 @@ p = Produit(u"Roulements", 250)
 p1 = Produit(u"piston", 10)
 p2 = Produit(u"pause pied", 20)
 
-rap = Rapport(u"Entre", 50, datetime(2011, 02, 01, 18, 20, 22, 88), 50)
+rap = Rapport(u"Entre", 30, datetime(2011, 02, 01, 18, 20, 22, 88), 50)
 rap.magasin = m
 rap.produit = p
+rap1 = Rapport(u"Entre", 10, datetime(2011, 05, 01, 04, 10, 12, 88), 50)
+rap1.magasin = m1
+rap1.produit = p1
+
+rap2 = Rapport(u"Entre", 150, datetime(2011, 03, 01, 18, 20, 22, 88), 50)
+rap2.magasin = m1
+rap2.produit = p2
 
 try:
     print "Enregistrement ..."
-    session.add_all((m, m1, p, p2, rap))
+    session.add_all((m, m1, p, p2, rap, rap1, rap2))
     print "commit ..."
     session.commit()
     print "Ok"
